68db00cce35ce9f555c99141f387ed476792e293
Brant Jerman <brantmichaeljerman@gmail.com>
2022-05-26 10:11:36 -0600

    Initial commit

---
+++ findTheRange.js
@@ -0,0 +1,12 @@
+const someCoolNumbers = [1, 4, 11, 2, 37, -4];
+
+const highest = Math.max(...someCoolNumbers);
+
+const lowest = Math.min(...someCoolNumbers);
+
+console.log("Highest Number: " + highest);
+
+console.log("Lowest Number: " + lowest);
+
+// Brother... you need to use an ARRAY!
+// Learn how to do that buddy
